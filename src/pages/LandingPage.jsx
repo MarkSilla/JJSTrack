44071c8bd3f4a2d362081ad41896d83c95551225
Lorenxo <lingadlourence06@gmail.com>
2026-02-16 13:10:59 +0800

    lines

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LandingNavbar from '../components/LandingNavbar'
 import Footer from '../components/Footer'
 
+
 const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
